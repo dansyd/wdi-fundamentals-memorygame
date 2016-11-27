790402bd7e41fe9596ca31f83c1a9c94ff6aa96e
Daniele Peviani <danielepeviani@Danieles-MacBook-Pro.local>
2016-11-27 12:35:16 +1100

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
+//Create 4 card variables, 2 with queen and 2 with king
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
